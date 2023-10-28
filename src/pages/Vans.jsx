4055e5d7e2ac53553caaa6a14a8e6f318d87c4f3
Junayed Jihad <junayedjihad48@gmail.com>
2023-10-29 00:14:34 +0600

    Single van page fixed

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -4,30 +4,35 @@ import Vancard from '../components/Vancard';
 
 const Vans = () => {
 
-    const [data, setData] = React.useState([])
+    const [data, setData] = React.useState()
     React.useEffect(() => {
       fetch("/api/vans")
         .then((response) => response.json())
         .then((res) => setData(res.vans));
     }, []);
 
-  const vanElements = data.map((item) => (
-
-    <Vancard
-        id={item.id}
-        key={item.id}
-        name={item.name}
-        type={item.type}
-        price={item.price}
-        imageUrl={item.imageUrl}
-      />
-
-  ));
+  let vanElements=''
+  if (data) {
+        vanElements = data.map((item) => (
+        <Vancard
+          id={item.id}
+          key={item.id}
+          name={item.name}
+          type={item.type}
+          price={item.price}
+          imageUrl={item.imageUrl}
+        />
+      ));
+    }
 
     return (
-      <div className='main'>
+      <div className="main">
+        {data ?
+          ( <div className=''>
         <h1 className='display-6 '>Explore our van options</h1>
         <div className="vancard-container mt-4">{ vanElements}</div>
+      </div>):<p>Loading...</p>
+        }
       </div>
     );
 };
